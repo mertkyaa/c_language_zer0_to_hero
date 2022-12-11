e01a3ead8273843226c820c08010daa89982bc95
mertkyaa <119807112+mertkyaa@users.noreply.github.com>
2022-12-11 15:51:05 +0300

    Create Ikı sayı.c

---
+++ Ikı sayı.c
@@ -0,0 +1,19 @@
+ #include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "ctype.h" 
+#include "math.h"
+
+int main(){
+	int a,b,c;
+	
+	printf("Enter your numbers: ");
+	scanf("%d\n%d",&a,&b);
+	
+	c = a+b;
+	printf("Result: %d",c);
+	
+	return 0;
+	
+	
+}
