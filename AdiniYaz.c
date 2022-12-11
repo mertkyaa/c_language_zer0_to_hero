18ca7a13cc67d163058da713581090cb04b274b4
mertkyaa <119807112+mertkyaa@users.noreply.github.com>
2022-12-11 14:09:18 +0300

    Update AdiniYaz.c

--- AdiniYaz.c
+++ AdiniYaz.c
@@ -1,1 +1,16 @@
 // BU programda adinizi yaziniz
+#include <stdio.h> 
+#include <stdlib.h>
+
+int main(){
+  char name[40];
+  char surname[30];
+  printf("Enter your name: ");
+  scanf("%s", name);
+  printf("Enter your surname: ");
+  scanf("%s", surname);
+  printf("%s %s",name,surname);
+  return 0;
+}
+
+  
